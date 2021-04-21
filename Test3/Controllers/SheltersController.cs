a9281d8096d20016d9f49f3af3a89fdf3100e820
Jakub Porębski <j.porebski0@gmail.com>
2021-04-21 17:04:56 +0200

    Added Scaffolded Controllers and Views for models: Pet, Post, Shelter, Vet

---
+++ Test3/Controllers/SheltersController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Petfinder.Models;
+
+namespace Petfinder.Controllers
+{
+    public class SheltersController : Controller
+    {
+        private readonly PetfinderContext _context;
+
+        public SheltersController(PetfinderContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Shelters
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Shelters.ToListAsync());
+        }
+
+        // GET: Shelters/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var shelter = await _context.Shelters
+                .FirstOrDefaultAsync(m => m.ShelterId == id);
+            if (shelter == null)
+            {
+                return NotFound();
+            }
+
+            return View(shelter);
+        }
+
+        // GET: Shelters/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Shelters/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ShelterId,Name,Password,Email,PhoneNumber,Localization")] Shelter shelter)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(shelter);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(shelter);
+        }
+
+        // GET: Shelters/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var shelter = await _context.Shelters.FindAsync(id);
+            if (shelter == null)
+            {
+                return NotFound();
+            }
+            return View(shelter);
+        }
+
+        // POST: Shelters/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ShelterId,Name,Password,Email,PhoneNumber,Localization")] Shelter shelter)
+        {
+            if (id != shelter.ShelterId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(shelter);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ShelterExists(shelter.ShelterId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(shelter);
+        }
+
+        // GET: Shelters/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var shelter = await _context.Shelters
+                .FirstOrDefaultAsync(m => m.ShelterId == id);
+            if (shelter == null)
+            {
+                return NotFound();
+            }
+
+            return View(shelter);
+        }
+
+        // POST: Shelters/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var shelter = await _context.Shelters.FindAsync(id);
+            _context.Shelters.Remove(shelter);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ShelterExists(int id)
+        {
+            return _context.Shelters.Any(e => e.ShelterId == id);
+        }
+    }
+}
