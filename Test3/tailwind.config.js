c69f5a57d038a567ef7964535ddcc7d7409edc61
Mixeroowsky <33137122+Mixeroowsky@users.noreply.github.com>
2021-05-18 16:55:55 +0200

    Better database with forms

---
+++ Test3/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    purge: {
+        enabled: false,
+        content: [
+            './Views/**/*.chstml'
+        ]
+    },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
